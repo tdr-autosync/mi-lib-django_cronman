37114fa70282eca3748a0d4260d57a63d4ca06c3
Adam Kawczynski <akawczynski@motoinsight.com>
2020-01-08 15:34:36 +0100

    add required config for python3

--- cronman/tests/settings.py
+++ cronman/tests/settings.py
@@ -15,11 +15,30 @@ DATABASES = {
 }
 
 INSTALLED_APPS = (
+    "django.contrib.messages",
     "django.contrib.contenttypes",
     "django.contrib.auth",
     "django.contrib.admin",
     "cronman",
 )
+
+MIDDLEWARE = (
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware"
+)
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "OPTIONS": {
+            "context_processors": [
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
 
 USE_I18N = True
 
