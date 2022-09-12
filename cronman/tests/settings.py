b8027c1c00db9c51ced70eb90207b21a5fb1f6b6
Eric Dantas <ericrommel@gmail.com>
2022-09-12 21:27:27 +0200

    MI-1505: Revert template loaders and add the required templates to MANIFEST file

--- cronman/tests/settings.py
+++ cronman/tests/settings.py
@@ -35,8 +35,6 @@ TEMPLATES = [
             "context_processors": [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "django.template.loaders.filesystem.Loader",
-                "django.template.loaders.app_directories.Loader",
             ],
         },
     },
